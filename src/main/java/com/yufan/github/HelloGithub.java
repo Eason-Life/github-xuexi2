bbbd9f1c4f743194de7f8a363fa3324dc426f59e
Eason-Life <240249923@qq.com>
2020-11-02 21:56:42 +0800

    new file input

---
+++ src/main/java/com/yufan/github/HelloGithub.java
@@ -0,0 +1,13 @@
+package com.yufan.github;
+
+/**
+ * @author YuFan
+ * @date 2020/11/2 21:48
+ * @jdk version 1.8.0_251
+ */
+public class HelloGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Fighting");
+    }
+}
